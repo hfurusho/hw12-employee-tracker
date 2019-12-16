84a457aa563fb107c9ba37931cbd49ba2d9005ba
Kami <51175640+hfurusho@users.noreply.github.com>
2019-12-15 23:16:46 -0800

    Corrected getFullEmployeesTable() to use left joins.

--- assets/queries.js
+++ assets/queries.js
@@ -131,9 +131,9 @@ function getFullEmployeesTable() {
     SELECT e.id, e.first_name, e.last_name, r.title AS role, 
     CONCAT(m.first_name, " ", m.last_name) AS manager 
     FROM employee AS e
-    JOIN role AS r
+    LEFT JOIN role AS r
     ON e.role_id = r.id
-    JOIN employee AS m
+    LEFT JOIN employee AS m
     ON e.manager_id = m.id`;
   return queryDB(query);
 }
